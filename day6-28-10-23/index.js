fd8023768d085b1b28c9225d1d1aa05e6cc75d1f
Chetan.k <141278557+Ch3tan88@users.noreply.github.com>
2023-10-28 16:10:41 +0530

    Add files via upload

---
+++ day6-28-10-23/index.js
@@ -0,0 +1,10 @@
+const bgImageEl = document.getElementById("photo-image");
+
+window.addEventListener("scroll", () => {
+  updateImage();
+});
+
+function updateImage() {
+  bgImageEl.style.opacity = 1 - window.pageYOffset / 900;
+  bgImageEl.style.backgroundSize = 160 - window.pageYOffset / 12 + "%";
+}
